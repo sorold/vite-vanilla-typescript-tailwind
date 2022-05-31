a19aa3f4d218663e2662ec15a25c88fc2de3c35f
Afonso Soroldoni <afonsosoroldoni@gmail.com>
2022-05-31 14:11:08 -0300

    Setup template.

---
+++ README.md
@@ -0,0 +1,4 @@
+## vite-vanilla-typescript-tailwind
+#Template for vite vanilla-ts with tailwindcss.
+You can just clone the repository and run `npm install`
+
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    content: [
+        "./*.html",
+        "./src/**/*.html"
+    ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
